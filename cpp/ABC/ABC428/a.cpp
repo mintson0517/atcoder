3e6b7313bfe4432b2e8c2d44a0848b616d996d8b
mintson0517 <mintson2020@gmail.com>
2025-11-02 23:35:25 +0800

    変更内容の説明

--- cpp/ABC/ABC428/a.cpp
+++ cpp/ABC/ABC428/a.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int S, A, B, X;
-    cin >> S >> A >> B >> X;
+    cin >> S >> A >> B >> X;  
     cout << S * ( (X / (A + B)) * A + min(X % (A + B), A) ) << endl;
     return 0;
 }
