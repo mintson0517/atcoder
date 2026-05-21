c5d5d58e6d3f4727fa235c27dd58c7acfdd69b7e
Mintson Huang/黃詺淙 <91959648+mintson0517@users.noreply.github.com>
2026-05-21 10:29:15 +0800

    Update b.cpp

---
+++ cpp/ABC/ABC458/b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int H, W;
+    cin >> H >> W;
+
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+
+            int cnt = 0;
+
+            // up
+            if (i > 0) cnt++;
+
+            // down
+            if (i < H - 1) cnt++;
+
+            // left
+            if (j > 0) cnt++;
+
+            // right
+            if (j < W - 1) cnt++;
+
+            cout << cnt;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
