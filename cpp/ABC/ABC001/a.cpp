dab9eee57ac2dacbf1c21b77d08b33e8ddcbb96b
mintson0517 <mintson1688@gmail.com>
2026-07-01 15:49:02 +0900

    add youtube shorts

--- cpp/ABC/ABC001/a.cpp
+++ cpp/ABC/ABC001/a.cpp
@@ -13,5 +13,6 @@ int main() {
 /*
 AtCoder問題文：https://atcoder.jp/contests/abc001/tasks/abc001_1
 YouTube解説動画：https://youtu.be/UPvXUcuGBvY
+YouTube Shorts：https://youtube.com/shorts/KymKBeLjrvE
 解説ブログ：https://yorunekomint.com/?p=187
 */
